76b63d5f3a1408557d161f53ca7eade6ba76a310
Evan Ugarte <evanuxd@gmail.com>
2021-02-16 08:42:32 -0800

    finish part 1

--- include/character_size.h
+++ include/character_size.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include <ostream>
+#include <unordered_map>
+
 namespace learning {
 enum class CharacterSize {
   kSmall,
   kMedium,
   kLarge,
 };
+
+std::unordered_map<CharacterSize, const char*> character_size_to_string({
+    {CharacterSize::kSmall, "small"},
+    {CharacterSize::kMedium, "medium"},
+    {CharacterSize::kLarge, "large"},
+});
 }  // namespace learning
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+#include "mongocxx/instance.hpp"
+
 #include "mongodb_handler.h"
 
+void print_result(const bool &result, const char* operation) {
+  std::cout << "the " << operation << (result ? " worked" : " didn't work.") << std::endl;
+}
+
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+  mongocxx::instance instance;
+  learning::MongoDbHandler mhandler;
+  bool result = mhandler.UpdateWins("602bed5efb52bf47da422532");
+  print_result(result, "update");
+  result = mhandler.RemoveCharacterFromDb("602bed5efb52bf47da422532");
+  print_result(result, "delete");
+
+  result = mhandler.UpdateWins("602bed5efb52bf47da422532");
+  print_result(result, "update");
+  result = mhandler.RemoveCharacterFromDb("602bed5efb52bf47da422532");
+  print_result(result, "delete");
 }
